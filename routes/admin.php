c5bc74c0b0cc5d661ef1ffd6de7dfc5919d80c53
vicholp <vlinerospardo@gmail.com>
2022-02-20 22:12:35 -0300

    fix: UserController not imported in admin.php

--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\UserController;
 use Rap2hpoutre\LaravelLogViewer\LogViewerController;
 
 /*
