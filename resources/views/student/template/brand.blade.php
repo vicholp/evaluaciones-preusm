3baee094d8e066aff3d30af9ab9290771ef5adc1
vicholpe <vlinerospardo@gmail.com>
2022-02-26 19:53:39 -0300

    feat: student view

---
+++ resources/views/student/template/brand.blade.php
@@ -0,0 +1,4 @@
+<div class="bg-black py-3 text-center  text-gray-200 flex items-center justify-center">
+  <span class="iconify-inline mr-2" data-icon="mdi:github"></span>
+  <a href="https://github.com/vicholp">Vicente Lineros Pardo - @vicholp</a>
+</div>
