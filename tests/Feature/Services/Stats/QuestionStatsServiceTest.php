2fdefc0374a7cc50de4f81fc47265f3b8014918e
vicholp <vlinerospardo@gmail.com>
2023-03-03 17:50:53 -0300

    feat: null index

--- tests/Feature/Services/Stats/QuestionStatsServiceTest.php
+++ tests/Feature/Services/Stats/QuestionStatsServiceTest.php
@@ -38,3 +38,25 @@ test('average score on pilot question', function () {
 
     expect($question->stats()->getAverageScore())->toBe($correct / $students->count());
 });
+
+test('null index', function () {
+    $question = Question::factory()->create();
+    addAlternativesToQuestion($question);
+
+    $students = Student::factory()->count(10)->create();
+
+    $null = 0;
+
+    foreach ($students as $student) {
+        $n = random_int(0, 1);
+
+        if ($n) {
+            $null++;
+            $student->attachAlternative($question->alternatives()->whereName('N/A')->first());
+        }else{
+            $student->attachAlternative($question->alternatives()->where('name', '!=', 'N/A')->inRandomOrder()->first());
+        }
+    }
+
+    expect($question->stats()->getNullIndex())->toBe($null / $students->count());
+});
