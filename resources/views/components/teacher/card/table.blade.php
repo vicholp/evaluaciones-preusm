0c23a8e0f644683adcb697c3a25617e8e00aa807
vicholp <vlinerospardo@gmail.com>
2023-03-03 17:48:26 -0300

    fix: color titulo tabla en modo oscuro

--- resources/views/components/teacher/card/table.blade.php
+++ resources/views/components/teacher/card/table.blade.php
@@ -34,7 +34,7 @@
 @props(['header'])
 
 <x-teacher.card.card :padding="false">
-  <div class="px-6 py-3 font-medium grid grid-cols-12 text-black text-opacity-90 bg-black bg-opacity-5">
+  <div class="px-6 py-3 font-medium grid grid-cols-12 text-black text-opacity-90 bg-black bg-opacity-5 dark:text-white">
     {{ $header }}
   </div>
   <div class="flex flex-col py-3">
