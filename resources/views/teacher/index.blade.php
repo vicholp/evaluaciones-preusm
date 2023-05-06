819f8a998864bd3eae530b3df773702d4697e14f
vicholp <vlinerospardo@gmail.com>
2023-05-06 05:33:20 -0400

    refactor: index view for teachers

--- resources/views/teacher/index.blade.php
+++ resources/views/teacher/index.blade.php
@@ -1,29 +1,32 @@
 @extends('teacher.template.main')
 
 @section('content')
-  <x-teacher.container>
-    <div class="col-span-12 ">
-      <x-teacher.card.card>
+  <x-base.layout.container>
+    <div class="col-span-12">
+      <x-base.card>
         @if ($questionnaireGroup)
-          <a href="{{ route('teacher.results.questionnaire-groups.show', $questionnaireGroup) }}">
-            {{ $questionnaireGroup->name }}
+          <a class="w-full py-3" href="{{ route('teacher.results.questionnaire-groups.show', $questionnaireGroup) }}">
+            {{ $questionnaireGroup->questionnaireClass->name . " " . $questionnaireGroup->name . " " . $questionnaireGroup->period->name }}
           </a>
-          <x-slot:footer>
-            <a href="{{ route('teacher.results.questionnaire-groups.index') }}">
+          <x-slot:footer :empty="true">
+            <a
+              href="{{ route('teacher.results.questionnaire-groups.index') }}"
+              class="text-black text-opacity-60 dark:text-white dark:text-opacity-60 py-1"
+            >
               {{ __('see all questionnaires') }}
             </a>
           </x-slot:footer>
         @else
           No hay cuestionarios
         @endif
-      </x-teacher.card.card>
+      </x-base.card>
     </div>
     <div class="col-span-12">
       <a href="{{ route('teacher.question-bank.index') }}">
-        <x-teacher.card.card>
-          {{ __('question bank') }}
-        </x-teacher.card.card>
+        <x-base.card>
+          {{ str(__('question bank'))->ucfirst() }}
+        </x-base.card>
       </a>
     </div>
-  </x-teacher.container>
+  </x-base.layout.container>
 @endsection
