02a33b54c561ec6872f9a7cf18e6dbb83ec0cee6
vicholp <vlinerospardo@gmail.com>
2023-05-28 16:11:27 -0400

    feat: dropdown vue component

---
+++ resources/js/components/shared/dropdown/dropdown.vue
@@ -0,0 +1,54 @@
+<template>
+  <Menu
+    as="div"
+    class="relative inline-block text-left"
+  >
+    <div>
+      <MenuButton
+        class="inline-flex w-full items-center
+          justify-center rounded-lg
+          bg-gray-300 p-3 dark:text-gray-100 text-gray-900 shadow
+          transition hover:bg-gray-400"
+      >
+        {{ title }}
+        <span
+          class="iconify text-xl"
+          data-icon="mdi:chevron-down"
+        />
+      </MenuButton>
+    </div>
+    <transition
+      enter-active-class="transition ease-out duration-100"
+      enter-from-class="transform opacity-0 scale-95"
+      enter-to-class="transform opacity-100 scale-100"
+      leave-active-class="transition ease-in duration-75"
+      leave-from-class="transform opacity-100 scale-100"
+      leave-to-class="transform opacity-0 scale-95"
+    >
+      <MenuItems class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <div class="py-1">
+          <slot />
+        </div>
+      </MenuItems>
+    </transition>
+  </Menu>
+</template>
+
+<script>
+import { Menu, MenuButton, MenuItems } from '@headlessui/vue';
+
+export default {
+  components: {
+    Menu,
+    MenuButton,
+    MenuItems,
+  },
+  props: {
+    title: {
+      type: String,
+      // required: true,
+      default: 'Button',
+    },
+  },
+};
+</script>
