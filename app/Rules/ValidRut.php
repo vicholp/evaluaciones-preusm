c20ea4c839d38bbc43ba7376216a7fe3b441eb7a
vicholp <vlinerospardo@gmail.com>
2023-05-31 16:55:55 -0400

    chore: fix style

--- app/Rules/ValidRut.php
+++ app/Rules/ValidRut.php
@@ -11,7 +11,6 @@ class ValidRut implements InvokableRule
      * Run the validation rule.
      *
      * @param string                                                                $attribute
-     * @param mixed                                                                 $value
      * @param \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString $fail
      *
      * @return void
