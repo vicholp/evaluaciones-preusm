1b83dc41a6424afeea7b84a0533e08eae0d1a3b9
vicholp <vlinerospardo@gmail.com>
2023-06-17 13:57:39 -0400

    chore: style

--- resources/views/components/teacher/instructions-questionnaire/general.blade.php
+++ resources/views/components/teacher/instructions-questionnaire/general.blade.php
@@ -5,32 +5,37 @@
     <h2 class="text-center text-2xl font-bold">
       ENSAYO GENERAL 1 – 2023
     </h2>
-    <h3 class="text-center font-bold my-3">
+    <h3 class="my-3 text-center font-bold">
       INSTRUCCIONES
     </h3>
 
-    <ol class="list-decimal flex flex-col gap-1">
+    <ol class="flex list-decimal flex-col gap-1">
       <li>
         Esta prueba contiene 65 preguntas, de las cuales puede que un maximo de 5 sean usadas para experimentacion
         y, por lo tanto, no se considerarán en el puntaje final de la prueba Las preguntas tienen 4 o 5 opciones
         de respuesta (A, B, C, D y E) <b> donde solo una de ellas es correcta. </b>
       </li>
       <li>
-        <b> Dispones de <x-teacher.questionnaire.duration :questionnaire="$questionnaire" /> para responder las 65 preguntas.</b> Este tiempo comienza después de la
+        <b> Dispones de
+          <x-teacher.questionnaire.duration :questionnaire="$questionnaire" /> para responder las 65 preguntas.
+        </b> Este tiempo comienza después de la
         lectura de las instrucciones, una vez contestadas las dudas y completados los datos de la hoja de respuestas
       </li>
       <li>
-        Las respuestas a las preguntas se marcan en la hoja de respuestas que se te entregó. Marca tu respuesta en la fila
+        Las respuestas a las preguntas se marcan en la hoja de respuestas que se te entregó. Marca tu respuesta en la
+        fila
         de celdillas <b> que corresponda al número de la pregunta que estás contestando. </b> Ennegrece completamente la
-        celdilla, tratando de no salirte de sus márgenes. Hazlo <b>exclusivamente</b> con lápiz de grafito Nº 2 o portaminas HB.
+        celdilla, tratando de no salirte de sus márgenes. Hazlo <b>exclusivamente</b> con lápiz de grafito Nº 2 o
+        portaminas HB.
       </li>
       <li>
         Puedes usar este folleto como borrador, pero <b>no olvides traspasar oportunamente tus respuestas a la hoja
-        de respuestas.</b> Ten presente que para la evaluación se considerarán exclusivamente las respuestas marcadas
+          de respuestas.</b> Ten presente que para la evaluación se considerarán exclusivamente las respuestas marcadas
         en dicha hoja
       </li>
       <li>
-        Cuida la hoja de respuestas. <b>No la dobles. No la manipules innecesariamente.</b> Escribe en ella solo los datos
+        Cuida la hoja de respuestas. <b>No la dobles. No la manipules innecesariamente.</b> Escribe en ella solo los
+        datos
         pedidos y las respuestas. Evita borrar para no deteriorarla. Si lo haces, límpiala de los residuos de goma
       </li>
       <li>
--- resources/views/teacher/question-bank/question/show.blade.php
+++ resources/views/teacher/question-bank/question/show.blade.php
@@ -4,38 +4,57 @@
   <x-teacher.container>
     <x-teacher.layout.title-bar
       :name="__('questions')"
-      :previus-route="route('teacher.question-bank.question-prototypes.index', ['where_subject_id' => request()->query('where_subject_id')])"
+      :previus-route="route('teacher.question-bank.question-prototypes.index', [
+          'where_subject_id' => request()->query('where_subject_id'),
+      ])"
     >
       <x-slot:actions>
         <x-teacher.action-button
-          method="POST" type="form"
+          method="POST"
+          type="form"
           :href="route('teacher.question-bank.question-prototypes.review', $question)"
           :body="__($reviewService->getReviewButtonName($user))"
         />
-        <x-teacher.action-button :href="route('teacher.question-bank.question-prototypes.edit', [$question, 'where_subject_id' => request()->query('where_subject_id')])" :body="__('edit')"/>
+        <x-teacher.action-button
+          :href="route('teacher.question-bank.question-prototypes.edit', [
+              $question,
+              'where_subject_id' => request()->query('where_subject_id'),
+          ])"
+          :body="__('edit')"
+        />
       </x-slot:actions>
     </x-teacher.layout.title-bar>
     <div class="col-span-12">
       <x-teacher.card.card>
         <x-teacher.card.list :divide="false">
-          <x-teacher.card.list-key-value :key="__('subject')" :value="$question->subject->name"/>
+          <x-teacher.card.list-key-value
+            :key="__('subject')"
+            :value="$question->subject->name"
+          />
           @if ($question->statement)
-            <x-teacher.card.list-key-value :key="__('statement')"
+            <x-teacher.card.list-key-value
+              :key="__('statement')"
               :value="$question->statement?->name"
               :link="route('teacher.question-bank.statement-prototypes.show', $question->statement)"
             />
           @endif
-          <x-teacher.card.list-key-value :key="__('name')" :value="$question->latest->name"/>
-          <x-teacher.card.list-key-value :key="__('description')" :value="$question->latest->description"/>
+          <x-teacher.card.list-key-value
+            :key="__('name')"
+            :value="$question->latest->name"
+          />
+          <x-teacher.card.list-key-value
+            :key="__('description')"
+            :value="$question->latest->description"
+          />
           <x-teacher.card.list-key-value :key="__('tags')">
             <x-slot:value>
               <div class="flex flex-wrap gap-2">
                 @forelse ($question->latest->tags as $tag)
-                  <div class="rounded py-1 px-2 bg-black bg-opacity-5 dark:bg-white dark:bg-opacity-5 text-sm">
+                  <div class="rounded bg-black bg-opacity-5 py-1 px-2 text-sm dark:bg-white dark:bg-opacity-5">
                     {{ __($tag->tagGroup->name) }}: {{ $tag->name }}
                   </div>
                 @empty
-                  <div class="rounded py-1 px-2 bg-black bg-opacity-5 dark:bg-white dark:bg-opacity-5 text-sm">
+                  <div class="rounded bg-black bg-opacity-5 py-1 px-2 text-sm dark:bg-white dark:bg-opacity-5">
                     <i>
                       {{ __('without tags') }}
                     </i>
@@ -44,8 +63,14 @@
               </div>
             </x-slot:value>
           </x-teacher.card.list-key-value>
-          <x-teacher.card.list-key-value :key="__('version')" :value="$question->latest->index"/>
-          <x-teacher.card.list-key-value :key="__('last reviewer')" :value="$reviewService->getLastReviewer()?->name ?? __('no reviewers')"/>
+          <x-teacher.card.list-key-value
+            :key="__('version')"
+            :value="$question->latest->index"
+          />
+          <x-teacher.card.list-key-value
+            :key="__('last reviewer')"
+            :value="$reviewService->getLastReviewer()?->name ?? __('no reviewers')"
+          />
         </x-teacher.card.list>
       </x-teacher.card.card>
     </div>
@@ -71,7 +96,10 @@
         </div>
         <div class="mt-3"></div>
         <x-teacher.card.list :divide="false">
-          <x-teacher.card.list-key-value :key="__('answer')" :value="$question->latest->answer"/>
+          <x-teacher.card.list-key-value
+            :key="__('answer')"
+            :value="$question->latest->answer"
+          />
         </x-teacher.card.list>
       </x-teacher.card.card>
     </div>
